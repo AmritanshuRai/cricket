ffd48b5fd19d7340f59209cc15d27a5bbc18f58d
Amritanshu rai <amritanshurai13@gmail.com>
2020-12-19 00:53:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './screens/HomePage';
+import PlayerProfile from './screens/PlayerProfile';
+import NotFound from './screens/404';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <Router>
+        <Header />
+
+        <Route path='/player/:id' component={PlayerProfile} />
+        <Route path='/404' component={NotFound} />
+
+        <Route path='/' component={HomePage} exact />
+      </Router>
+    </ErrorBoundary>
   );
 }
 
